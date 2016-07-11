0ad5c1db937787b45438d3a85d042a5acb81266e
Sebastian <sebastian@Sebastians-Mac-Pro.local>
2016-07-11 10:45:37 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,217 @@
+# jQuery confirmation plugin
+
+Simple confirmation plugin for jQuery.
+
+[Demo is available here](http://ssd-confirm.ssdtutorials.com/)
+
+## Installation
+
+To make the plugin work you need to include jQuery and styles from within `ssd-confirm.css` file as well as either minified or src version of the plugin.
+
+```
+<link href="./assets/css/ssd-confirm.css" rel="stylesheet">
+
+<script src="./node_modules/jquery/dist/jquery.js"></script>
+<script src="./src/jquery.ssd-confirm.js"></script>
+```
+
+You also need to add the following html towards the bottom of your page structure (all `data` attributes are configurable):
+
+### Confirmation dialog
+
+```
+<div data-ssd-confirm>
+
+    <div data-ssd-confirm-block="remove">
+
+        <p data-ssd-confirm-content></p>
+
+        <nav>
+
+            <span data-button-wrapper>
+
+                <span
+                    class="small alert button"
+                    data-ssd-confirm-yes
+                    data-trigger
+                >YES</span>
+
+                <span
+                    class="small alert disabled button hide"
+                    data-processing
+                ><i class="fa fa-spinner fa-spin fa-fw"></i></span>
+
+            </span>
+
+            <span class="small button" data-ssd-confirm-no>NO</span>
+
+        </nav>
+
+    </div>
+
+</div>
+
+<div data-ssd-confirm-overlay></div>
+```
+
+### Example list of records
+
+```
+<table class="table-list">
+
+    <thead>
+        <tr>
+            <th>
+                Name
+            </th>
+            <th class="col-1">
+                <span
+                    class="small secondary disabled button"
+                >
+                    <i class="fa fa-trash fa-fw"></i>
+                </span>
+            </th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr data-row>
+            <td>
+                Remove row after confirming
+            </td>
+            <td>
+                <span
+                    class="small button"
+                    data-ssd-confirm-trigger="remove"
+                    data-ssd-confirm-message="Are you sure you wish to remove this record?<br />There is no undo!"
+                    data-ssd-confirm-url="/remove.json"
+                    data-ssd-confirm-behaviour="removeRow"
+                >
+                    <i class="fa fa-trash fa-fw"></i>
+                </span>
+            </td>
+        </tr>
+        <tr data-row>
+            <td>
+                Reload after confirming
+            </td>
+            <td>
+                <span
+                    class="small button"
+                    data-ssd-confirm-trigger="remove"
+                    data-ssd-confirm-message="Are you sure you wish to remove this record?<br />There is no undo!"
+                    data-ssd-confirm-url="/remove.json"
+                    data-ssd-confirm-behaviour="reload"
+                >
+                    <i class="fa fa-trash fa-fw"></i>
+                </span>
+            </td>
+        </tr>
+        <tr data-row>
+            <td>
+                Reload after confirming
+            </td>
+            <td>
+                <span
+                    class="small button"
+                    data-ssd-confirm-trigger="remove"
+                    data-ssd-confirm-message="Are you sure you wish to remove this record?<br />There is no undo!"
+                    data-ssd-confirm-url="/remove.json"
+                    data-ssd-confirm-behaviour="reload"
+                >
+                    <i class="fa fa-trash fa-fw"></i>
+                </span>
+            </td>
+        </tr>
+    </tbody>
+
+</table>
+```
+
+Considering the above, here's how the data attributes from the two code blocks are referring to each other
+
+
+- `data-ssd-confirm-trigger`: Associated value (in this case `remove`) corresponds to the `data-ssd-confirm-block`. You can have number of different purpose blocks.
+- `data-ssd-confirm-message`: Message that will be injected into the `data-ssd-confirm-content`.
+- `data-ssd-confirm-url`: End point to be called when the `Yes` button within the confirmation dialog is triggered.
+- `data-ssd-confirm-behaviour`: Method that will be executed after successful call.
+
+
+## Instantiating plugin
+
+Call plugin on the instance of the trigger you wish to apply it to.
+
+```
+$('[data-ssd-confirm-trigger]').ssdConfirm();
+```
+
+## Options
+
+```
+dataType: 'ssd-confirm-trigger',            // trigger 'data-*' attribute - corresponds to the 'dataConfirmBlock'.
+dataMessage: 'ssd-confirm-message',         // trigger 'data-*' attribute - corresponds to the 'dataConfirmContent'.
+dataUrl: 'ssd-confirm-url',                 // trigger 'data-*' attribute that stores the url / uri to be called when 'Yes' button is triggered.
+dataBehaviour: 'ssd-confirm-behaviour',     // trigger 'data-*' attribute that indicates what method should be executed after successful call.
+
+dataConfirmWrapper: 'ssd-confirm',          // dialog wrapper 'data-*' attribute.
+dataConfirmBlock: 'ssd-confirm-block',      // dialog block type 'data-*' attribute - corresponds to 'dataType'.
+dataConfirmContent: 'ssd-confirm-content',  // dialog content 'data-*' attribute - corresponds to 'dataMessage'.
+
+dataButtonWrapper: 'button-wrapper',        // dialog button wrapper 'data-*' attribute - used when you want to show 'pending' button when ajax call is being made.
+dataButtonTrigger: 'trigger',               // dialog trigger 'data-*' attribute - to be used with 'Yes' button.
+dataButtonProcessing: 'processing',         // dialog processing 'data-*' attribute - to be used with button that swaps 'Yes' button when ajax call is being made.
+
+dataButtonYes: 'ssd-confirm-yes',           // dialog 'yes' button 'data-*' attribute.
+dataButtonNo: 'ssd-confirm-no',             // dialog 'no' button 'data-*' attribute.
+
+dataRecordWrapper: 'row',                   // list record wrapper 'data-*' attribute.
+
+dataOverlay: 'ssd-confirm-overlay',         // overlay 'data-*' attribute.
+
+classActive: 'active',                      // dialog css class used to reveal the confirmation dialog (by default it slides it down).
+classShow: 'show',                          // dialog / overlay css class to reveal given block of the confirmation dialog and overlay.
+classHide: 'hide',                          // dialog css class to hide 'yes' / 'processing' buttons.
+
+extendBlocks: {},                           // object that can extend blocks (currently only 'remove').
+extendBehaviours: {}                        // object that can extend behaviours (currently only 'reload' and 'removeRow').
+```
+
+## Behaviours
+
+Currently there are only 2 behaviours, but you can add more by adding them to the `extendBehaviours` option.
+
+```
+reload: Reloads page.
+removeRow: Removes the container of the record. This option requires `dataRecordWrapper` to be applied to the container of your record (default `data-row`), which should contain the trigger button. Example
+
+
+<tr data-row>
+    <td>
+        Record name
+    </td>
+    <td>
+        <span
+            class="small button"
+            data-ssd-confirm-trigger="remove"
+            data-ssd-confirm-message="Are you sure you wish to remove this record?<br />There is no undo!"
+            data-ssd-confirm-url="/remove.json"
+            data-ssd-confirm-behaviour="removeRow"
+        >
+            <i class="fa fa-trash fa-fw"></i>
+        </span>
+    </td>
+</tr>
+```
+
+Behaviour methods take 3 arguments:
+
+```
+data:               // data returned from ajax call
+trigger:            // instance of the 'yes' button
+properties:         // object containing:
+
+    url:            // url / uri to be called
+    behaviour:      // behaviour name
+    originator:     // instance of the button that triggered the dialog
+    processing:     // instance of the 'processing' button if exists - otherwise 'null'
+```
---
+++ src/jquery.ssd-confirm.js
@@ -0,0 +1,355 @@
+/*
+ * ssdConfirm jQuery plugin
+ * Examples and documentation at: https://github.com/sebastiansulinski/ssd-confirm
+ * Copyright (c) 2016 Sebastian Sulinski
+ * Version: 0.0.1 (11-JUL-2016)
+ * Licensed under the MIT.
+ * Requires: jQuery v1.9 or later
+ */
+;(function(window, $, undefined) {
+
+    "use strict";
+
+    $.fn.ssdConfirm = function (options) {
+
+        "use strict";
+
+        var settings = $.extend({
+
+                dataType: 'ssd-confirm-trigger',
+                dataMessage: 'ssd-confirm-message',
+                dataUrl: 'ssd-confirm-url',
+                dataBehaviour: 'ssd-confirm-behaviour',
+
+                dataConfirmWrapper: 'ssd-confirm',
+                dataConfirmBlock: 'ssd-confirm-block',
+                dataConfirmContent: 'ssd-confirm-content',
+
+                dataButtonWrapper: 'button-wrapper',
+                dataButtonTrigger: 'trigger',
+                dataButtonProcessing: 'processing',
+
+                dataButtonYes: 'ssd-confirm-yes',
+                dataButtonNo: 'ssd-confirm-no',
+
+                dataRecordWrapper: 'row',
+
+                dataOverlay: 'ssd-confirm-overlay',
+
+                classActive: 'active',
+                classShow: 'show',
+                classHide: 'hide',
+
+                extendBlocks: {},
+                extendBehaviours: {}
+
+            }, options);
+
+        var Block = $.extend({
+
+            remove: function(trigger, properties) {
+
+                "use strict";
+
+                Confirm.showOverlay();
+
+                $('[' + Confirm.dataAttr(settings.dataConfirmBlock) + '="remove"]')
+                    .addClass(settings.classShow)
+                    .find('[' + Confirm.dataAttr(settings.dataButtonYes) + ']')
+                    .data('behaviour', properties.behaviour)
+                    .data('url', properties.url)
+                    .data('originator', trigger)
+                    .addBack()
+                    .find('[' + Confirm.dataAttr(settings.dataConfirmContent) + ']')
+                    .html(properties.message);
+
+                $('[' + Confirm.dataAttr(settings.dataConfirmWrapper) + ']').addClass(settings.classActive);
+
+            }
+
+        }, settings.extendBehaviours);
+
+        var Behaviour = $.extend({
+
+            reload: function(data, trigger, properties) {
+
+                "use strict";
+
+                window.location.reload(true);
+
+            },
+
+            removeRow: function(data, trigger, properties) {
+
+                "use strict";
+
+                Confirm.hide();
+
+                properties.originator
+                    .closest('[' + Confirm.dataAttr(settings.dataRecordWrapper) + ']')
+                    .fadeOut(200, function() {
+                        $(this).remove();
+                    });
+
+            }
+
+        }, settings.extendBehaviours);
+
+        var Action = {
+
+            hasButtonSwap: function(trigger, processing) {
+
+                "use strict";
+
+                return (
+                    ! Confirm.isEmpty(trigger) &&
+                    ! Confirm.isEmpty(processing)
+                );
+
+            },
+
+            showProcessing: function(trigger, processing) {
+
+                "use strict";
+
+                if ( ! this.hasButtonSwap(trigger, processing))  {
+                    return;
+                }
+
+                trigger.addClass(settings.classHide);
+                processing.removeClass(settings.classHide);
+
+            },
+
+            hideProcessing: function(trigger, processing) {
+
+                "use strict";
+
+                if ( ! this.hasButtonSwap(trigger, processing))  {
+                    return;
+                }
+
+                processing.addClass(settings.classHide);
+                trigger.removeClass(settings.classHide);
+
+            },
+
+            call: function() {
+
+                "use strict";
+
+                $(document).on(
+                    'click',
+                    '[' + Confirm.dataAttr(settings.dataButtonYes) + ']',
+                    function(event) {
+
+                        Confirm.prevent(event);
+
+                        var trigger = $(this),
+                            wrapper = trigger.closest('[' + Confirm.dataAttr(settings.dataButtonWrapper) + ']'),
+                            properties = {
+                                url: trigger.data('url'),
+                                behaviour: trigger.data('behaviour'),
+                                originator: trigger.data('originator'),
+                                processing: wrapper.length > 0 ?
+                                        wrapper.find('[' + Confirm.dataAttr(settings.dataButtonProcessing) + ']') :
+                                        null
+                            };
+
+                        Action.showProcessing(trigger, properties.processing);
+
+                        $.ajax({
+                            url: properties.url,
+                            dataType: 'json',
+                            success: function(data) {
+
+                                Behaviour[properties.behaviour](data, trigger, properties);
+
+                            },
+                            error: function (jqXHR, textStatus, errorThrown) {
+
+                                Action.hideProcessing(trigger, properties.processing);
+
+                                throw new Error(errorThrown);
+                            }
+                        });
+
+                    }
+                );
+
+            },
+
+            init: function() {
+
+                "use strict";
+
+                this.call();
+
+            }
+
+        };
+
+        var Confirm = {
+
+            isEmpty: function(value) {
+
+                "use strict";
+
+                return (
+                    typeof value === 'undefined' ||
+                    value === undefined ||
+                    value === '' ||
+                    value === false ||
+                    value === null
+                )
+
+            },
+
+            prevent: function(event) {
+
+                "use strict";
+
+                event.preventDefault();
+                event.stopPropagation();
+
+            },
+
+            dataAttr: function(name) {
+
+                "use strict";
+
+                return 'data-' + name;
+
+            },
+
+            showOverlay: function() {
+
+                "use strict";
+
+                $('[' + Confirm.dataAttr(settings.dataOverlay) + ']').addClass(settings.classShow);
+
+            },
+
+            hideOverlay: function() {
+
+                "use strict";
+
+                $('[' + Confirm.dataAttr(settings.dataOverlay) + ']').removeClass(settings.classShow);
+
+            },
+
+            overlay: function() {
+
+                "use strict";
+
+                $('[' + Confirm.dataAttr(settings.dataOverlay) + ']').on(
+                    'click',
+                    function(event) {
+
+                        Confirm.prevent(event);
+                        Confirm.hide();
+
+                    }
+                );
+
+            },
+
+            hide: function() {
+
+                "use strict";
+
+                $('[data-ssd-confirm]')
+                    .removeClass(settings.classActive)
+                    .find('[' + Confirm.dataAttr(settings.dataConfirmBlock) + ']')
+                    .removeClass(settings.classShow)
+                    .find('[' + Confirm.dataAttr(settings.dataButtonWrapper) + '] a, [' + Confirm.dataAttr(settings.dataButtonWrapper) + '] span')
+                    .removeAttr('data-url')
+                    .removeAttr('data-behaviour')
+                    .removeAttr('data-originator')
+                    .addBack()
+                    .find('[' + Confirm.dataAttr(settings.dataButtonProcessing) + ']').addClass(settings.classHide)
+                    .addBack()
+                    .find('[' + Confirm.dataAttr(settings.dataButtonTrigger) + ']').removeClass(settings.classHide);
+
+                this.hideOverlay();
+
+            },
+
+            buttonNo: function() {
+
+                "use strict";
+
+                $('[' + Confirm.dataAttr(settings.dataButtonNo) + ']').on(
+                    'click',
+                    function(event) {
+
+                        Confirm.prevent(event);
+                        Confirm.hide();
+
+                    }
+                );
+
+            },
+
+            hasMethod: function(method, object) {
+
+                "use strict";
+
+                return (
+                    ! Confirm.isEmpty(method) &&
+                    object.hasOwnProperty(method)
+                );
+
+            },
+
+            trigger: function(instance) {
+
+                "use strict";
+
+                instance.each(function() {
+
+                    $(this).on(
+                        'click',
+                        function(event) {
+
+                            Confirm.prevent(event);
+
+                            var trigger = $(this),
+                                properties = {
+                                    type: trigger.data(settings.dataType),
+                                    url: trigger.data(settings.dataUrl),
+                                    message: trigger.data(settings.dataMessage),
+                                    behaviour: trigger.data(settings.dataBehaviour)
+                                };
+
+                            if ( ! Confirm.hasMethod(properties.type, Block)) {
+                                return;
+                            }
+
+                            Block[properties.type](trigger, properties);
+
+                        }
+                    );
+
+                });
+
+            },
+
+            init: function (instance) {
+
+                "use strict";
+
+                this.trigger(instance);
+                this.buttonNo();
+                this.overlay();
+
+                Action.init();
+
+            }
+
+        };
+
+        return Confirm.init(this);
+    };
+
+})(window, window.jQuery);
